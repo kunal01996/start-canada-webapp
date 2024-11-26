db7cfd6f60c56ea7c9000ba328a82a78b5ef432c
Kunal Singh <kunalsingh20102006@gmail.com>
2024-11-26 15:52:12 +0530

    fix(yml): yml file update

--- startpm2.sh
+++ startpm2.sh
@@ -1,10 +1,11 @@
-# Check if 'webapp' is running in PM2
+# checking if 'webapp' running in pm2
+
 PM2_EXIST=$(if pm2 list 2> /dev/null | grep -q webapp; then echo "Yes" ; else echo "No" ; fi)
 
-if [ $PM2_EXIST = "Yes" ] ; then
+if [ $PM2_EXIST = Yes ] ; then
     pm2 restart webapp
-    echo "Restarted webapp."
+    echo "Restart webapp."
 else
-    pm2 start node ./server.js --name webapp
-    echo "Started webapp with 'node ./server.js'."
+    pm2 start npm --name webapp -- run start -- -p 3000
+    echo "Started webapp."
 fi
